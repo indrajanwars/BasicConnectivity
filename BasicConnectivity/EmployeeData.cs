e5819818b2912fa48c18a06bd3934dce48b5b39c
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-09-21 16:11:16 +0700

    Create EmployeeData.cs

---
+++ BasicConnectivity/EmployeeData.cs
@@ -0,0 +1,18 @@
+﻿namespace BasicConnectivity;
+public class EmployeeData
+{
+    public int Id { get; set; }
+    public string Full_Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public int Salary { get; set; }
+    public string Department_Name { get; set; }
+    public string Street_Address { get; set; }
+    public string Country_Name { get; set; }
+    public string Region_Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id} - {Full_Name} - {Email} - {Phone} - {Salary} - {Department_Name} - {Street_Address} - {Country_Name} - {Region_Name}";
+    }
+}
