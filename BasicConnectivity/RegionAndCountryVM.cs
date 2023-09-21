4d1526c7d155c94fb3bfe2471654638897f35bba
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-09-21 16:11:04 +0700

    Create RegionAndCountryVM.cs

---
+++ BasicConnectivity/RegionAndCountryVM.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicConnectivity;
+public class RegionAndCountry
+{
+    public int CountryId { get; set; }
+    public string CountryName { get; set; }
+    public string RegionName { get; set; }
+    public int RegionId { get; set; }
+    public string City { get; set; }
+
+    public override string ToString()
+    {
+        return $"{CountryId} - {CountryName} - {RegionName} - {RegionId} - {City}";
+    }
+}
