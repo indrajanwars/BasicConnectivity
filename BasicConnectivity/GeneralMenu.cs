727701702e4b086029ef75f23f2bfe01ba1804a4
indrajanwars <indrajanwarsetyadi@gmail.com>
2023-09-21 16:09:30 +0700

    Update GeneralMenu.cs

--- BasicConnectivity/GeneralMenu.cs
+++ BasicConnectivity/GeneralMenu.cs
@@ -4,11 +4,32 @@ public class GeneralMenu
 {
     public static void List<T>(List<T> items, string title)
     {
-        Console.WriteLine($"List of {title}");
+        Console.WriteLine($"\nList of {title}");
         Console.WriteLine("---------------");
         foreach (var item in items)
         {
             Console.WriteLine(item.ToString());
+        }
+    }
+
+    public static void Single<T>(T item, string title)
+    {
+        Console.WriteLine($"\nList of {title}");
+        Console.WriteLine("---------------");
+        Console.WriteLine(item.ToString());
+    }
+
+    public static void Transaction(string result)
+    {
+        int.TryParse(result, out int res);
+        if (res > 0)
+        {
+            Console.WriteLine("Transaction completed successfully");
+        }
+        else
+        {
+            Console.WriteLine("Transaction failed");
+            Console.WriteLine(result);
         }
     }
 }
